83c79a4124623f253023dc8a44a1ab2bc2813eaa
mrfchang03 <74283811+mrfchang03@users.noreply.github.com>
2024-02-14 12:51:12 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,20 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
+      "ME NO GUSTA",
+      "But maybe yes",
+      "LIKE PLEASE MAN FUCK",
+      "YES OR NO TFT",
+      ":(",
+      "FUCK U",
       "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "okie killing myself",
+      "gg is 8th",
+      "i dont even want you anymore at this point",
+      "please",
       ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
+      "please please please",
+      "dang, killing myself",
       "No :(",
     ];
 
@@ -37,14 +37,14 @@ export default function Page() {
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
       {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <img src="https://www.icegif.com/wp-content/uploads/valentines-day-icegif-30.gif" />
           <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
         </>
       ) : (
         <>
           <img
             className="h-[200px]"
-            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+            src="https://pa1.aminoapps.com/7103/a0559486633f84d0a56d1a4975ba041f556b8b34r1-370-286_hq.gif"
           />
           <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
           <div className="flex items-center">
